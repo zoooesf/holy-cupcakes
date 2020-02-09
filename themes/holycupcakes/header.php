b840dcce2f7a65ef415e1de488472649a7ab9898
Suleman Mulla <mullasuleman12@gmail.com>
2020-02-09 04:31:28 -0500

    added grid-container in header. disabled site title

--- themes/holycupcakes/header.php
+++ themes/holycupcakes/header.php
@@ -31,17 +31,17 @@
 		<header id="masthead" class="site-header">
 			<div class="site-branding">
 
-				<div class="top-bar" id="site-navigation">
+				<div class="top-bar grid-container" id="site-navigation">
 					<div class="top-bar-left">
 						<?php
 						the_custom_logo();
 						if (is_front_page() && is_home()) :
 						?>
-							<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+							<!-- <h1 class="site-title"><a href="<?php //echo esc_url(home_url('/')); ?>" rel="home"><?php //bloginfo('name'); ?></a></h1> -->
 						<?php
 						else :
 						?>
-							<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+							<!-- <p class="site-title"><a href="<?php //echo esc_url(home_url('/')); ?>" rel="home"><?php //bloginfo('name'); ?></a></p> -->
 						<?php
 						endif;
 						// $holy_cupcakes_description = get_bloginfo('description', 'display');
