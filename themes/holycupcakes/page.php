6ab51ccaef9d6fe271abeaeef234e7371df19400
Suleman Mulla <mullasuleman12@gmail.com>
2020-02-09 06:05:21 -0500

    added grid to default page template

--- themes/holycupcakes/page.php
+++ themes/holycupcakes/page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -15,25 +16,27 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
+		<div class="grid-container">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+			<?php
+			while (have_posts()) :
+				the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+				get_template_part('template-parts/content', 'page');
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if (comments_open() || get_comments_number()) :
+					comments_template();
+				endif;
 
-		endwhile; // End of the loop.
-		?>
+			endwhile; // End of the loop.
+			?>
+		</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
