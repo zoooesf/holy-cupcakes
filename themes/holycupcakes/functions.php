968ca96ec604513aae3895de76647ed88b35771a
Suleman Mulla <mullasuleman12@gmail.com>
2020-02-09 01:49:56 -0500

    fixing the style functions order. moved styles to style.css. style.css is primary css now

--- themes/holycupcakes/functions.php
+++ themes/holycupcakes/functions.php
@@ -174,7 +174,6 @@ add_action('widgets_init', 'holy_cupcakes_widgets_init');
  */
 function holy_cupcakes_scripts()
 {
-	wp_enqueue_style('holycupcakes-style', get_stylesheet_uri());
 
 	// enqueue foundation styles
 	wp_enqueue_style(
@@ -182,18 +181,26 @@ function holy_cupcakes_scripts()
 		get_template_directory_uri() . '/assets/css/vendors/foundation.min.css',
 		array()
 	);
+
+	// enqueue style.css
+	wp_enqueue_style('holycupcakes-style', get_stylesheet_uri());
 	
+
+	// TODO: REMOVE ALL THE COMMENTED STYLE ENQUEUE FUNCTION BELOW IF STYLES ARE WORKING PROPERLY
+
+
 	// enqueue custom styles
-	wp_enqueue_style(
-		'custom-style',
-		get_template_directory_uri() . '/assets/css/custom.css',
-		array()
-	);
-	// enqueue custom styles
-	wp_enqueue_style(
-		'custom-style',
-		get_template_directory_uri() . '/style.css',
-	);
+	// wp_enqueue_style(
+	// 	'custom-style',
+	// 	get_template_directory_uri() . '/assets/css/custom.css',
+	// 	array()
+	// );
+	// // enqueue custom styles
+	// wp_enqueue_style(
+	// 	'custom-style',
+	// 	get_template_directory_uri() . '/style.css',
+	// );
+	
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
 	}
