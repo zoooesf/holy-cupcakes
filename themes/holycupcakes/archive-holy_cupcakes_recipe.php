15ea8d5205df5ac198df3fa2c1fe3f549dcd6476
Zoe <zoe.ferguson@hotmail.com>
2020-03-30 18:55:15 -0400

    comments

--- themes/holycupcakes/archive-holy_cupcakes_recipe.php
+++ themes/holycupcakes/archive-holy_cupcakes_recipe.php
@@ -38,32 +38,31 @@ get_header();
 						while (have_posts()) :
 							the_post();
 						?>
+						<!-- individual recipe cards -->
 							<div class="cell card large-5 medium-5 small-10 recipeCard">
 								<?php
 								if (has_post_thumbnail()) {
 								?>
+								<!-- recipe featured image -->
 									<div class="thumbnail-img">
 										<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_post_thumbnail_caption(); ?>" />
 									</div>
 								<?php
 								} ?>
+								<!-- recipe title -->
 								<div class="card-section recipeExcerpt">
 									<h3><?php the_title(); ?> </h3>
 									<p>
+										<!-- recipe excerpt -->
 										<?php
 										the_excerpt();
 										?>
 									</p>
-									<a class="recipeBtn" href="<?php echo get_post_permalink(); ?>">continue reading</a>
+									<!-- button linking to recipe -->
+									<a class="recipeBtn" href="<?php echo get_post_permalink(); ?>">start baking</a>
 								</div>
 							</div>
 					<?php
-						/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-						// get_template_part('template-parts/content', get_post_type());
 
 						endwhile;
 
