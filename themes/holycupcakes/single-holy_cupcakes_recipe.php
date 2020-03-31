0029d61a45b8d5e57bdad5a16ba5eaaa2a3b4a12
Suleman Mulla <mullasuleman12@gmail.com>
2020-03-31 06:15:37 -0400

    updated grid structure on single recipe template

--- themes/holycupcakes/single-holy_cupcakes_recipe.php
+++ themes/holycupcakes/single-holy_cupcakes_recipe.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: single-recipe
  * Template Post Type: post, page, recipe
@@ -14,9 +15,11 @@ get_header();
 <div id="primary" class="content-area">
 	<main id="main" class="site-main">
 		<!-- banner image for directly below menu -->
-	<div class="wp-block-cover alignfull has-background-dim" style="background-image:url(http://holycupcake.local/wp-content/uploads/2020/02/brooke-lark.jpg)"><div class="wp-block-cover__inner-container">
-<p class="has-text-align-center has-large-font-size"></p>
-</div></div>
+		<div class="wp-block-cover alignfull has-background-dim" style="background-image:url(http://holycupcake.local/wp-content/uploads/2020/02/brooke-lark.jpg)">
+			<div class="wp-block-cover__inner-container">
+				<p class="has-text-align-center has-large-font-size"></p>
+			</div>
+		</div>
 		<div class="grid-container">
 			<div class="grid-x" id="recipe">
 				<div class="large-4 grid-margin-x show-for-large sidebar-box">
@@ -25,26 +28,30 @@ get_header();
 					get_sidebar();
 					?>
 				</div>
-		<section class="large-8 medium-12 grid-x align-justify blog">
-			<!-- blog content -->
-		<?php
-		while ( have_posts() ) :
-			the_post();
-		?>
-			<h1><?php echo get_the_title();?></h1>
-			<hr class="hSeparator">
-			<p><?php get_template_part( 'template-parts/content', get_post_type() );?><p><?php
+				<section class="large-8 medium-12 grid-x align-justify blog">
+					<!-- blog content -->
+					<?php
+					while (have_posts()) :
+						the_post();
+					?>
+						<h1><?php echo get_the_title(); ?></h1>
+						<hr class="hSeparator">
+						<?php get_template_part('template-parts/content', get_post_type()); ?>
+						<?php
 
-			the_post_navigation();
+						// navigation for more posts next and previous
+						the_post_navigation(array(
+							'prev_text' => 'Previous: %title',
+							'next_text' => 'Next: %title',
+						));
 
-			
-
-		endwhile; // End of the loop.
-		?>
-</section>
-		</main><!-- #main -->
-		</div>
-	</div><!-- #primary -->
+					endwhile; // End of the loop.
+					?>
+				</section>
+			</div><!-- .grix-x -->
+		</div><!-- .grid-container -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
