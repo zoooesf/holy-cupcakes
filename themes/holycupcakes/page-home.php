fa7dd84127bed4a8368bc86c8aab43e2e7bb7245
Suleman Mulla <mullasuleman12@gmail.com>
2020-03-31 06:27:47 -0400

    commented page-home.php

--- themes/holycupcakes/page-home.php
+++ themes/holycupcakes/page-home.php
@@ -17,6 +17,7 @@ get_header();
 
 <section id="primary" class="content-area">
 	<main id="main" class="site-main">
+		<!-- container for home page contents -->
 		<div class="grid-container">
 			<?php
 
